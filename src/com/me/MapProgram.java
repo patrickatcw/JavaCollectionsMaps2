6e1c27ed7c572b1f2a0aa1bc08ee16f8eb2c46de
patrickatcw <pj8392@yahoo.com>
2018-05-05 16:06:17 -0400

    replace key

--- src/com/me/MapProgram.java
+++ src/com/me/MapProgram.java
@@ -49,7 +49,7 @@ public class MapProgram {
         System.out.println("==================================================================");
 
         //step 1 removing a key
-        languages.remove("Lisp");
+        //languages.remove("Lisp");  commented out for step 2
 
         //and we can remove a key if it is mapped to a certain value
         if(languages.remove("Algol", "an algorithmic language")) {
@@ -65,11 +65,29 @@ public class MapProgram {
             */
         }
 
+        //step 2 comment out line 52 above then...
+        //replace
+        //System.out.println(languages.replace("Lisp", "a functional programming language" +
+        //"with imperative features"));
+
+        //step 3 an if statement replace, changed from code above and code below commented out
+        if(languages.replace("Lisp", "Thererin lies MADNESS!!", "a functioanl programming language" +
+                "with imperative features")) {
+            System.out.println("Lisp replaced");
+        }else {
+            System.out.println("Lisp was not replaced");
+            //run
+        }
+
+
+        //System.out.println(languages.replace("Scala", "this will not be added"));
+        //run
 
         //keyset, prints entire keyset
         for(String key: languages.keySet()){
             System.out.println(key + " : " + languages.get(key));
         }
+
 
     }
 
